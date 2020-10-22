8bcb7610b83fd47ff276fe3edf9c8830ec4ba7b4
Atomic <71985841+AtomicSystem@users.noreply.github.com>
2020-10-22 06:00:54 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Skin Changer'
+
+version '1.0.3'
+
+client_scripts {
+	'locale.lua',
+	'locales/br.lua',
+	'locales/de.lua',
+	'locales/en.lua',
+	'locales/es.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/pl.lua',
+	'locales/sv.lua',
+	'locales/tr.lua',
+	'config.lua',
+	'client/accordant_arms.lua',
+	'client/main.lua'
+}
+
+exports {
+	'getAccordantArms'
+}
+
+client_script '@exm_debug/Shareds/ToLoad.lua'
